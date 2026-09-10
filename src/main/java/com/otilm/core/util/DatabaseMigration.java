0a11045b862550428c20655a4ced197e509a9708
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-09-10 13:29:43 +0200

    Grant the acme role the credential gate the dereference skeleton always passes
    Signed-off-by: klaraf755 <80590912+klaraf755@users.noreply.github.com>

--- src/main/java/com/otilm/core/util/DatabaseMigration.java
+++ src/main/java/com/otilm/core/util/DatabaseMigration.java
@@ -76,7 +76,7 @@ public class DatabaseMigration {
         V202607031200__CreateAttributeContentResolverUserAndPermissions(1703465896, false),
         V202608071000__RegistrationSubjectDnNormalizedMigration(612311821),
         V202608311000__GrantRaProfileMembersToAttributeContentResolver(-2072942854),
-        V202609101100__GrantSecretContentToAcme(778426271);
+        V202609101100__GrantSecretContentToAcme(-1308461994);
 
         private final int checksum;
 
